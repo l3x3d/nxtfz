477114b41122e9a0e6779b6f443677ae5d7dd106
Jameson Taylor <56190805+l3x3d@users.noreply.github.com>
2023-09-06 14:22:36 +0000

    Update 3 files

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} font-inter antialiased bg-gradient-to-r from-lime-200 to-teal-900 text-gray-900 tracking-tight`}>
+      <body className={`${inter.variable} font-inter antialiased bg-white dark:bg-stone-900 text-gray-900 tracking-tight`}>
         <div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
           <Header />
           {children}
