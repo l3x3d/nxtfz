d33f9451d328eb5891e3f666c838ac1c10a2701d
Jameson Taylor <56190805+l3x3d@users.noreply.github.com>
2023-09-06 02:35:15 +0000

    Update footer.tsx and logo.tsx

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -3,16 +3,9 @@ import Link from 'next/link'
 export default function Logo() {
   return (
     <Link href="/" className="block" aria-label="Cruip">
-      <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-        <defs>
-          <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="footer-logo">
-            <stop stopColor="#4FD1C5" offset="0%" />
-            <stop stopColor="#81E6D9" offset="25.871%" />
-            <stop stopColor="#338CF5" offset="100%" />
-          </radialGradient>
-        </defs>
-        <rect width="32" height="32" rx="16" fill="url(#footer-logo)" fillRule="nonzero" />
-      </svg>
+     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 102 155" fill="none">
+  <path d="M0.333344 77.3333V154.667H20.3333H40.3333L40.6 126.267L41 98L66.7333 97.6L92.3333 97.3333V80.6667V64H66.3333H40.3333V48.6667V33.3333H71H101.667V16.6667V4.64916e-06H51H0.333344V77.3333Z" fill="#84fa07"/>
+</svg>
     </Link>
   )
 }
